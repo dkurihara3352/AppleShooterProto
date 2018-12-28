7a42e0239efc5b9dec68bcc7332cf05256252fd1
Daisuke Kurihara <dkurihara3352@gmail.com>
2018-12-29 01:59:59 +0900

    test build commence

--- Assets/Proto/Scripts/WaypointEventSystem/TargetSpawnWaypointEventDrawer/TargetSpawnWaypointEventDrawer.cs
+++ Assets/Proto/Scripts/WaypointEventSystem/TargetSpawnWaypointEventDrawer/TargetSpawnWaypointEventDrawer.cs
@@ -7,29 +7,13 @@ namespace AppleShooterProto{
 	public class TargetSpawnWaypointEventDrawer : MonoBehaviour {
 		public AbsShootingTargetSpawnPointGroupAdaptor[] targetSpawnPointGroupAdaptors;
 		public PCWaypointCurveAdaptor pcWaypointCurveAdaptor;
-		void Update(){
-			if(targetSpawnPointGroupAdaptors != null)
-				thisSpawnPointAdaptors = CollectSpawnPointAdaptors();
-			UpdateReadiness();
-		}
-		void UpdateReadiness(){
-			if(thisIsReady){
-				if(
-					targetSpawnPointGroupAdaptors == null ||
-					pcWaypointCurveAdaptor == null
-				)
-					thisIsReady = false;
-			}else{
-				if(
-					targetSpawnPointGroupAdaptors != null &&
-					pcWaypointCurveAdaptor != null &&
-					thisSpawnPointAdaptors != null
-				)
-					thisIsReady = true;
+
+		bool thisIsReady = false;
+		IShootingTargetSpawnPointAdaptor[] thisSpawnPointAdaptors{
+			get{
+				return CollectSpawnPointAdaptors();
 			}
 		}
-		bool thisIsReady = false;
-		IShootingTargetSpawnPointAdaptor[] thisSpawnPointAdaptors;
 		IShootingTargetSpawnPointAdaptor[] CollectSpawnPointAdaptors(){
 			List<IShootingTargetSpawnPointAdaptor> resultList = new List<IShootingTargetSpawnPointAdaptor>();
 			foreach(IShootingTargetSpawnPointGroupAdaptor groupAdaptor in targetSpawnPointGroupAdaptors)
@@ -37,18 +21,23 @@ namespace AppleShooterProto{
 					resultList.AddRange(groupAdaptor.GetAdaptors());
 			return resultList.ToArray();
 		}
+
+
 		public Color lineColor;
-		// void OnDrawGizmos(){
-		// 	if(thisIsReady){
-		// 		Gizmos.color = lineColor;
-		// 		foreach(IShootingTargetSpawnPointAdaptor pointAdaptor in thisSpawnPointAdaptors){
-		// 			Vector3 position = pointAdaptor.GetPosition();
-		// 			float eventPoint = pointAdaptor.GetEventPoint();
-		// 			Vector3 eventPointPosition = pcWaypointCurveAdaptor.GetPositionOnCurve(eventPoint);
-					
-		// 			Gizmos.DrawLine(position, eventPointPosition);
-		// 		}
-		// 	}
-		// }
+		void OnDrawGizmos(){
+			Gizmos.color = lineColor;
+			foreach(IShootingTargetSpawnPointAdaptor pointAdaptor in thisSpawnPointAdaptors){
+				if(pointAdaptor != null){
+					Vector3 position = pointAdaptor.GetPosition();
+					float eventPoint = pointAdaptor.GetEventPoint();
+					if(pcWaypointCurveAdaptor != null){
+						Vector3 eventPointPosition = pcWaypointCurveAdaptor.GetPositionOnCurve(eventPoint);
+						
+						Gizmos.DrawLine(position, eventPointPosition);
+					}
+				}
+			}
+			
+		}
 	}
 }
